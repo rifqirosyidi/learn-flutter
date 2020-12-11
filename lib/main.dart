1aa44f16915fa9dc0332658b95d9b2c491a6cfc6
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2020-12-11 16:05:36 +0700

    network image

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,18 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello Universe'),
+          title: Text('Hello Universe '),
           backgroundColor: Colors.deepOrange,
           centerTitle: true,
         ),
         body: Center(
-            child: Text(
-                "Hello Universe",
-                style: TextStyle(
-                  fontFamily: 'AnticSlab',
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.0,
-                  color: Colors.grey[500]
-                ),
+            child: Image(
+              image: NetworkImage('https://images.unsplash.com/photo-1586348943529-beaae6c28db9?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTh8fHxlbnwwfHx8&w=1000&q=80')
             )
         ),
         floatingActionButton: FloatingActionButton(
